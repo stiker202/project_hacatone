370f91487041ad815405250a42617cead16ce17f
stiker202 <ajdarv20@gmail.com>
2026-07-05 11:02:44 +0300

    я хз как это сработает, но должно просто обновиться

--- script.py
+++ script.py
@@ -2,11 +2,5 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "привет"
-@app.route('/hello')
-def hello_world():
-    return "Hello World!"
-@app.route('/user/<username>')
-def user(username):
-    return f"page of {username}"
+    return 
 app.run(debug=True)
